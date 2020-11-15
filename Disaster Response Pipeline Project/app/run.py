94e718fa3f6bbc40fcdde2bc76d15a48d660c2f9
datagy <54583565+datagy@users.noreply.github.com>
2020-11-15 11:40:17 -0500

    Update run.py

--- Disaster Response Pipeline Project/app/run.py
+++ Disaster Response Pipeline Project/app/run.py
@@ -26,11 +26,13 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('messages', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+# with open('models\classifer.pkl','rb') as f:
+#     model=pickle.load(f)
+model = joblib.load("..\models\classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
